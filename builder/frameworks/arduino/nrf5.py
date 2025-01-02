b7d6455a41ad847f32052179d6314a2be08778f8
klingenbergh <hampus@klingtech.no>
2025-01-02 23:26:21 +0100

    Updated frameworkdir

--- builder/frameworks/arduino/nrf5.py
+++ builder/frameworks/arduino/nrf5.py
@@ -28,7 +28,7 @@ env = DefaultEnvironment()
 platform = env.PioPlatform()
 board = env.BoardConfig()
 
-FRAMEWORK_DIR = platform.get_package_dir("framework-arduinonordicnrf5")
+FRAMEWORK_DIR = platform.get_package_dir("framework-arduinoadafruitnrf52-seeed")
 assert isdir(FRAMEWORK_DIR)
 
 env.Append(
